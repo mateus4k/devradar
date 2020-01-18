48fb0c7c0556d4fc6be749e12aa64f34d759489c
Mateus <mateus4k@protonmail.ch>
2020-01-18 19:00:53 -0300

    :rocket: Socket.io! :rocket:

--- mobile/src/pages/Main/index.js
+++ mobile/src/pages/Main/index.js
@@ -8,6 +8,7 @@ import {
 import { MaterialIcons } from "@expo/vector-icons";
 
 import api from "../../services/api";
+import { connect, disconnect, subscribeToNewDevs } from "../../services/socket";
 
 import styles from "./styles";
 
@@ -39,6 +40,18 @@ function Main({ navigation }) {
     loadInitialPosition();
   }, []);
 
+  useEffect(() => {
+    subscribeToNewDevs(dev => setDevs([...devs, dev]));
+  }, [devs]);
+
+  function setupWebsocket() {
+    disconnect();
+
+    const { latitude, longitude } = currentRegion;
+
+    connect(latitude, longitude, techs);
+  }
+
   async function loadDevs() {
     const { latitude, longitude } = currentRegion;
 
@@ -51,6 +64,7 @@ function Main({ navigation }) {
     });
 
     setDevs(data);
+    setupWebsocket();
   }
 
   function handleRegionChanged(region) {
